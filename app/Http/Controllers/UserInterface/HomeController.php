6e16af6b454dac0ddfe21e97f0e46b02adcb821e
Arif Billah Shobuz <98076974+arifbillahshobuz507@users.noreply.github.com>
2026-02-07 22:35:03 +0600

    category frontend

--- app/Http/Controllers/UserInterface/HomeController.php
+++ app/Http/Controllers/UserInterface/HomeController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\UserInterface;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\models\Category;
 
 class HomeController extends Controller
 {
     //home
-    public function home()
+public function home()
     {
-    return view('userInterface.layout.master');
+        // Fetch only root categories (no parent) for the homepage
+        $categories = Category::whereNull('parent_id')
+            ->orderBy('priority', 'asc')
+            ->orderBy('name', 'asc')
+            ->take(10) // Limit to 10 categories for the slider
+            ->get();
 
+        return view('userInterface.app', compact('categories'));
     }
+    
 }
